d6be3e226ccc85499a9e6c6d0bf901404ddc19aa
Dustin Palmatier <dustinpalmatier@icloud.com>
2022-12-25 12:02:04 -0500

    Showing Posts on Profile now

--- src/routes/profile/@[username]/+page.server.ts
+++ src/routes/profile/@[username]/+page.server.ts
@@ -5,13 +5,61 @@ import type { PageLoad } from './$types';
 export const load = (async ({ params }) => {
 	const member = await db.member.findFirst({
 		where: { username: params.username.toLowerCase() },
-		include: { comments: true, posts: true }
+		select: {
+			posts: {
+				select: {
+					category: { select: { id: true, name: true, status: true } },
+					comments: {
+						select: { author: { select: { name: true, id: true, status: true } } }
+					},
+					content: true,
+					description: true,
+					id: true,
+					status: true,
+					subject: true,
+					createdAt: true,
+					updatedAt: true,
+					tags: {
+						select: {
+							id: true,
+							tag: { select: { id: true, name: true, status: true } },
+							status: true,
+							createdAt: true,
+							updatedAt: true
+						}
+					}
+				}
+			},
+
+			comments: {
+				select: {
+					post: {
+						select: {
+							category: { select: { id: true, name: true, status: true } },
+							status: true,
+							subject: true,
+							id: true
+						}
+					},
+					id: true,
+					status: true,
+					content: true,
+					createdAt: true,
+					updatedAt: true
+				}
+			},
+
+			id: true,
+			name: true,
+			role: true,
+			status: true,
+			username: true,
+			createdAt: true,
+			updatedAt: true
+		}
 	});
 
 	if (!member) throw error(404);
-
-	delete member.emailAddress;
-	delete member.passwordHash;
 
 	return member;
 }) satisfies PageLoad;
